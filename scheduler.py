257a868a3a59c51c63836c16e02f040d60eed5b1
nutchapon49-cyber <nutchapon49@icloud.com>
2026-06-02 12:33:56 +0700

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -24,7 +24,8 @@ class MedScheduler:
                     key = f"{t}"
                     if key not in seen:
                         seen.add(key)
-                        schedule.every().day.at(t).do(
+                        schedule.every().day.at(t, "Asia/Bangkok").do(
+
                             self._send_reminders_at, t
                         ).tag("med")
 
